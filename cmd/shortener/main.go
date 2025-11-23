6edffe8915c5285e243c75fdd824b9d6b86b82a8
olga <olgagliko@icloud.com>
2025-11-22 16:44:56 -0800

    increment2 - done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,35 +8,78 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"time"
+	"regexp"
+	"strings"
 )
 
-// Map для хранения оригиналов и коротких версий URL
-var urlsMap map[string]string
+const (
+	route            = "http://"
+	methodNotAllowed = "Метод не поддерживается"
+	incorrectRoute   = "Некорректный маршрут"
+)
 
-const route = "http://"
+var urlsMap = make(map[string]string)
 
-func initArgs() {
-	urlsMap = make(map[string]string)
-	rand.Seed(time.Now().UnixNano())
+func SetValue(key, value string) {
+	urlsMap[key] = value
 }
 
-//type ShortLink struct {
-//	URL     string `json:"url"`
-//	ShortID string `json:"short_id"`
-//}
+func GetValue(key string) (string, bool) {
+	val, ok := urlsMap[key]
+	return val, ok
+}
 
-func generateRandomStr(n int) (string, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	if err != nil {
-		return "", errors.New("ошибка генерации случайного значения")
+func ResetGlobalMap() {
+	for k := range urlsMap {
+		delete(urlsMap, k)
 	}
-	s := base64.StdEncoding.EncodeToString(b)
-	return s[:n], nil
 }
 
-// POST-обработчик: создает новую запись с коротким URL
+func main() {
+	ResetGlobalMap()
+	registrationRoutes()
+
+	log.Println("Запуск сервера на http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func registrationRoutes() {
+	http.HandleFunc("/", createShortURLHandler)
+	http.HandleFunc("/{id}", redirectHandler)
+	http.HandleFunc("/.*", badRouteHandler)
+}
+
+func createShortURLHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	handleCreateShortURL(w, r)
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	path := r.URL.Path
+	// Разбираем путь и получаем ID
+	parts := strings.Split(path, "/")
+	if len(parts) != 2 || len(parts[1]) != 8 {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	if match, _ := regexp.MatchString("[0-9]", parts[1]); !match {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	handleRedirect(w, r)
+}
+
+func badRouteHandler(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r)
+}
+
 func handleCreateShortURL(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
@@ -47,54 +90,38 @@ func handleCreateShortURL(w http.ResponseWriter, r *http.Request) {
 
 	originalURL := bytes.TrimSpace(body)
 
-	// Генерация уникального короткого идентификатора
 	shortID, err := generateRandomStr(8)
 	if err != nil {
 		http.Error(w, "Ошибка генерации сокращённого URL", http.StatusInternalServerError)
 		return
 	}
 
-	// Сохраняем ссылку в карте
-	urlsMap[shortID] = string(originalURL)
+	SetValue(shortID, string(originalURL))
 
-	// Возвращаем успешный ответ с новым URL
 	responseURL := route + r.Host + "/" + shortID
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(responseURL))
 }
 
-// GET-обработчик: переадресация по короткому URL
 func handleRedirect(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Path[1:]
-	log.Println("Получили Get request:", id)
-	// Извлекаем оригинал из карты
-	redirectURL, ok := urlsMap[id]
+	redirectURL, ok := GetValue(id)
 	if !ok {
 		http.Error(w, "Ссылка не найдена", http.StatusNotFound)
 		return
 	}
 
-	// Переадресация
 	w.Header().Set("Location", redirectURL)
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
-func main() {
-	initArgs()
-	// Регистрация обработчиков маршрутов
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			handleCreateShortURL(w, r)
-		default:
-			http.Error(w, "Метод не поддерживается", http.StatusBadRequest)
-		}
-	})
-
-	http.HandleFunc("/{id}", handleRedirect)
-
-	// Запускаем сервер
-	log.Println("Запуск сервера на http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func generateRandomStr(n int) (string, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", errors.New("ошибка генерации случайного значения")
+	}
+	s := base64.StdEncoding.EncodeToString(b)
+	return s[:n], nil
 }
